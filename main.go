f3f8f7319a52f0ac9f84850cbedf9c20435d7793
BlueLizard-at <kento0511greeniguana@icloud.com>
2025-02-27 01:04:54 +0900

    [fix] GetRoute実装

--- main.go
+++ main.go
@@ -6,21 +6,26 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/sushigon-dev/sagaicle-backend/GetRoute"
 	"github.com/sushigon-dev/sagaicle-backend/GetTags"
+	//"github.com/sushigon-dev/sagaicle-backend/GetRoute"
 )
 
 func main() {
 
-	db, err := sql.Open("sqlite3", "./database.db")
+	db, err := sql.Open("sqlite3", "./GetRoute.db")
 
 	if err != nil {
 		log.Fatal("Database connection error:", err)
 	}
 	defer db.Close()
 
-	router := gin.Default()
+	tagHandler := &GetTags.TagHandler{DB: db}
+	routeHandler := &GetRoute.RouteHandler{DB: db}
 
-	router.GET("/api/tags", GetTags.GetTags(db))
+	router := gin.Default()
+	router.GET("/api/tags", tagHandler.GetTags)
+	router.GET("/api/route/:id", routeHandler.GetRoute)
 
 	router.Run("localhost:8080")
 }
