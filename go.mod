98b61c486f7a551bc350d8ca2236cb7312486f58
BlueLizard-at <kento0511greeniguana@icloud.com>
2025-02-26 21:12:08 +0900

    [add] GetTagsの実装 動作確認用のmain関数付き

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module myapp
+module sagaicle-backend
 
 go 1.23.6
 
--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import (
 	"database/sql"
 	"log"
 
+	"sagaicle-backend/GetTags"
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/sushigon-dev/sagaicle-backend/GetTags"
 )
 
 func main() {
