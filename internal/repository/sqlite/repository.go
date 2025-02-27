5a4114d4554ffdce89cccaf9a27770928cd8f708
rayfiyo <rayfiyorex@gmail.com>
2025-02-28 06:55:14 +0900

    feat: 🍻 リポジトリ層 ルートの実装

--- internal/repository/sqlite/repository.go
+++ internal/repository/sqlite/repository.go
@@ -1,8 +1,13 @@
 package sqlite
 
 import (
-	// "github.com/sushigon-dev/sagaicle/internal/domain"
+	// "database/sql"
+	// "encoding/json"
+	// "time"
+
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
+	"github.com/sushigon-dev/sagaicle/internal/domain"
 )
 
 // タグに関する操作を抽象化したインターフェース
@@ -10,12 +15,18 @@ type TagsRepository interface {
 	GetTags() ([]string, error)
 }
 
-// SQLiteRepository は sqlx.DB を利用したリポジトリ実装です。
+// ルートに関する操作を抽象化したインターフェース
+type RouteRepository interface {
+	CreateRoute(route *domain.Route) error
+	GetRouteByID(id uuid.UUID) (*domain.Route, error)
+}
+
+// sqlx.DB を利用したリポジトリ実装
 type SQLiteRepository struct {
 	db *sqlx.DB
 }
 
-// NewSQLiteRepository は新たなリポジトリインスタンスを生成します。
+// 新たなリポジトリインスタンスを生成
 func NewSQLiteRepository(db *sqlx.DB) *SQLiteRepository {
 	return &SQLiteRepository{db: db}
 }
