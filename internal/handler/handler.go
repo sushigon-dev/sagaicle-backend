b534fb7f3afad3811d2690ccca8488de7f6ba187
rayfiyo <rayfiyorex@gmail.com>
2025-02-28 08:07:00 +0900

    feat: 🍻 それっぽくしてみた

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -12,13 +12,21 @@ import (
 
 // サービス層を利用するハンドラー層のエントリーポイント
 type Handler struct {
-	tagsService service.TagsService
+	checkpointsService service.CheckpointsService
+	likesService       service.LikesService
+	routesService      service.RoutesService
+	tagsService        service.TagsService
+	usersService       service.UsersService
 }
 
 // 新たなハンドラーを生成
-func NewHandler(tagsService service.TagsService) *Handler {
+func NewHandler(checkpointsService service.CheckpointsService, likesService service.LikesService, routesService service.RoutesService, tagsService service.TagsService, usersService service.UsersService) *Handler {
 	return &Handler{
-		tagsService: tagsService,
+		checkpointsService: checkpointsService,
+		likesService:       likesService,
+		routesService:      routesService,
+		tagsService:        tagsService,
+		usersService:       usersService,
 	}
 }
 
@@ -26,4 +34,24 @@ func NewHandler(tagsService service.TagsService) *Handler {
 func (h *Handler) RegisterRoutes(router *gin.Engine) {
 	// タグ
 	router.GET("/api/tags", h.GetTags)
+
+	// ルート
+	router.POST("/api/search", h.SearchRoutes)
+	router.POST("/api/routes", h.CreateRoute)
+	router.GET("/api/routes/:route_id", h.GetRouteByID)
+
+	// いいね
+	router.GET("/api/routes/:route_id/like", h.IsLiked)
+	router.POST("/api/routes/:route_id/like", h.LikeRoute)
+	router.DELETE("/api/routes/:route_id/like", h.DislikeRoute)
+
+	// チェックポイント
+	router.GET("/api/routes/:route_id/checkpoints", h.GetVisitedCheckpoints)
+	router.POST("/api/routes/:route_id/checkpoints/:checkpoint_index/visit", h.VisitCheckpoint)
+
+	// ユーザー（認証系）
+	router.POST("/api/auth/register", h.Register)
+	router.POST("/api/auth/login", h.Login)
+	router.GET("/api/auth/me", h.Whoami)
+	router.DELETE("/api/auth/logout", h.Logout)
 }
