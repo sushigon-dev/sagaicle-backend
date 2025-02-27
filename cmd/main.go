2aa2a6851be4a2a4009bc632c7fbef1e4da67ffc
rayfiyo <rayfiyorex@gmail.com>
2025-02-28 06:31:38 +0900

    feat: 🍻 いけたにょん！！

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3" // SQLite ドライバ
@@ -30,12 +31,21 @@ func main() {
 	}
 
 	// リポジトリ、サービス、ハンドラーの初期化
-	tagsRepo := repository.NewTagsRepository(db)
+	tagsRepo := repository.NewSQLiteTagsRepository(db)
 	tagsService := service.NewRouteService(tagsRepo)
 	h := handler.NewHandler(tagsService)
 
 	// Gin ルーターの作成
 	router := gin.Default()
+
+	// CORS ミドルウェアの設定
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost", "https://sushigon-dev.github.io"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	// ルートの登録
 	h.RegisterRoutes(router)
