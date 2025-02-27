5601ba3de4eac4a4d81a61de9e2f570cdd89f0d9
rayfiyo <rayfiyorex@gmail.com>
2025-02-28 05:58:48 +0900

    feat: 🍻 サービス層できたらしい

---
+++ internal/service/service.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"database/sql"
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/sushigon-dev/sagaicle/internal/domain"
+	repository "github.com/sushigon-dev/sagaicle/internal/repository/sqlite"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// RouteService はルート関連のユースケースを扱います。
+type RouteService interface {
+	GetTags() ([]string, error)
+}
+
+type routeService struct {
+	repo repository.TagsRepository
+}
+
+// NewRouteService は新たな RouteService を生成します。
+func NewRouteService(repo repository.TagsRepository) RouteService {
+	return &routeService{repo: repo}
+}
+
+// GetTags はリポジトリから全てのタグを取得します。
+func (s *routeService) GetTags() ([]string, error) {
+	return s.repo.GetTags()
+}
