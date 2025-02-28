d00c44f8b7fb9a52576592ed7c0842da7c8719aa
rayfiyo <rayfiyorex@gmail.com>
2025-02-28 11:53:48 +0900

    feat: 🍻 エラーの定型文追加

---
+++ utils/errors/errors.go
@@ -0,0 +1,9 @@
+package errors
+
+const (
+	InvalidFormat       = "リクエストや入力値の形式が正しくないです💦"
+	MissingParameter    = "パラメータが足りないです💦"
+	Authentication      = "認証に失敗しました💦"
+	ResourceIssues      = "対象のリソースが見つからなかったり、すでに存在します💦"
+	InternalServerError = "内部サーバーエラーが発生しました💦"
+)
