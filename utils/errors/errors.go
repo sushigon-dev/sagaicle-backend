8d9408e1b09d65fdf0f5797fa3a0e879659b4a06
rayfiyo <rayfiyorex@gmail.com>
2025-02-28 19:01:11 +0900

    refactor: 💡 エラー変数名

--- utils/errors/errors.go
+++ utils/errors/errors.go
@@ -1,8 +1,8 @@
 package errors
 
 const (
-	InvalidFormat       = "リクエストや入力値の形式が正しくないです💦"
-	Authentication      = "認証に失敗しました💦"
-	ResourceIssues      = "対象のリソースが見つからなかったり、すでに存在します💦"
-	InternalServerError = "内部サーバーエラーが発生しました💦"
+	Authentication = "認証に失敗しました💦"
+	InternalServer = "内部サーバーエラーが発生しました💦"
+	InvalidFormat  = "リクエストや入力値の形式が正しくないです💦"
+	Resource       = "対象のリソースが見つからなかったり、すでに存在します💦"
 )
