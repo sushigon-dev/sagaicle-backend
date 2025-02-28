85c903337547597636f319fec1e974bdc4957aa0
rayfiyo <rayfiyorex@gmail.com>
2025-02-28 14:19:40 +0900

    feat: 🍻 ドメインの RouteSummary

--- internal/domain/route.go
+++ internal/domain/route.go
@@ -19,6 +19,19 @@ type Route struct {
 	Images           []string     `json:"images"`            // ルートの画像URL (1〜6件)
 	Map              string       `json:"map"`               // マップのURL
 	Checkpoints      []Checkpoint `json:"checkpoints"`       // ルートに含まれるチェックポイントの一覧
-	Likes            int          `json:"likes"`             // いいねの総数
 	UpdateAt         time.Time    `json:"update_at"`         // 更新日時（YYYY/MM/DD）
+	Likes            int          `json:"likes"`             // いいねの総数
+}
+
+// ルート概要を表現
+type RouteSummary struct {
+	ID          uuid.UUID `json:"id"`          // ルートのユニークID
+	Title       string    `json:"title"`       //  タイトル (1〜20文字)
+	Description string    `json:"description"` // 短い説明 (1〜60文字)
+	Distance    float64   `json:"distance"`    // 距離（0以上の値）
+	Time        int       `json:"time"`        // 時間（0以上の値、単位は分など）
+	Tags        []string  `json:"tags"`        // ルートに関連付けられたタグ
+	Likes       int       `json:"likes"`       // いいね総数（一覧画面では静的なのでAPIを使わない）
+	Image       string    `json:"image"`       // ルートの画像URL（詳細の0番目）
+	UpdateAt    time.Time `json:"update_at"`   // 更新日時（YYYY/MM/DD）
 }
