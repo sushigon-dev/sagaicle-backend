be37d803567d604b134178936a3e51f0d9f3ad31
rayfiyo <rayfiyorex@gmail.com>
2025-02-28 15:07:46 +0900

    fix: 🐛 ロギングうまくいかないん

--- utils/config/db.go
+++ utils/config/db.go
@@ -13,14 +13,14 @@ func DB() *sqlx.DB {
 	// sqlx を利用して SQLite データベースに接続
 	db, err := sqlx.Open("sqlite3", "./sagaicle.db")
 	if err != nil {
-		logger.LogError(err, "DBのオープンに失敗")
+		logger.Error(err, "DBのオープンに失敗")
 		log.Fatalf("SQLite データベースのオープンに失敗しました: %v", err)
 	}
 
 	// init.sql の内容をファイルから読み込む
 	sqlBytes, err := os.ReadFile("init.sql")
 	if err != nil {
-		logger.LogError(err, "init.sqlの読み込みに失敗")
+		logger.Error(err, "init.sqlの読み込みに失敗")
 		log.Fatal(err)
 	}
 	sqlStatements := string(sqlBytes)
@@ -28,13 +28,13 @@ func DB() *sqlx.DB {
 	// 初期化スクリプトを実行
 	_, err = db.Exec(sqlStatements)
 	if err != nil {
-		logger.LogError(err, "初期化スクリプト実行に失敗")
+		logger.Error(err, "初期化スクリプト実行に失敗")
 		log.Fatal(err)
 	}
 
 	// データベース接続確認
 	if err := db.Ping(); err != nil {
-		logger.LogError(err, "DBの接続確認失敗")
+		logger.Error(err, "DBの接続確認失敗")
 		log.Fatalf("SQLite データベースへの接続確認に失敗しました: %v", err)
 	}
 
