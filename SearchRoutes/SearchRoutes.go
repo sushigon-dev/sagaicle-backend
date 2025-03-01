eb4ad983d6f8499ee5294cd971bfb76354a627f5
BlueLizard-at <kento0511greeniguana@icloud.com>
2025-03-01 21:08:05 +0900

    feat: 🍻 時間と距離での検索

--- SearchRoutes/SearchRoutes.go
+++ SearchRoutes/SearchRoutes.go
@@ -30,6 +30,25 @@ type SearchRequest struct {
 }
 
 // ルート情報のレスポンス用構造体
+type PreRoute struct {
+	ID                string  `json:"id"`
+	Title             string  `json:"title"`
+	Description       string  `json:"description"`
+	Full_description  string  `json:"full_description"`
+	Distance          float64 `json:"distance"`
+	Time              int     `json:"time"`
+	Tags              string  `json:"tags"`
+	Total_Checkpoints int     `json:"total_checkpoints"`
+	Images            string  `json:"images"`
+	Map               string  `json:"map"`
+	Name              string  `json:"name"`
+	Lat               int     `json:"lat"`
+	Ing               int     `json:"ing"`
+	Route_ID          string  `json:"route_id"`
+	UpdateAt          string  `json:"update_at"`
+	Image             string  `json:"image"`
+	Likes             int     `json:"likes"`
+}
 type Route struct {
 	ID          string  `json:"id"`
 	Title       string  `json:"title"`
@@ -61,30 +80,106 @@ func (h *SearchHandler) SearchRoutes(c *gin.Context) {
 		return
 	}
 
-	// SQL クエリの作成
-	query := `SELECT ID, TITLE, DESCRIPTION, DISTANCE, TIME, TAGS, LIKES, IMAGE, UPDATE_AT FROM routes WHERE 1=1`
-	args := []interface{}{}
+	switch req.SearchOption {
+	case "AND":
+		query := "SELECT * FROM routes WHERE (DISTANCE BETWEEN ? AND ?) AND (TIME BETWEEN ? AND ?)"
+		args := []interface{}{req.Distance.Min, req.Distance.Max, req.Time.Min, req.Time.Max}
 
-	//OR検索に対応させる
-	// 距離のフィルタ
-	if req.Distance.Min >= 0 {
-		query += " AND distance >= ?"
-		args = append(args, req.Distance.Min)
-	}
-	if req.Distance.Max >= 0 {
-		query += " AND distance <= ?"
-		args = append(args, req.Distance.Max)
+		// 取得制限
+		limit := 12
+		if req.Limit >= 1 && req.Limit <= 60 {
+			limit = req.Limit
+		}
+		query += " LIMIT ?"
+		args = append(args, limit)
+
+		rows, err := h.DB.Query(query, args...)
+		if err != nil {
+			log.Println("Query Error:", err)
+			c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "Database query error"})
+			return
+		}
+		defer rows.Close()
+
+		var routes []Route
+		for rows.Next() {
+			var route Route
+			var preroute PreRoute
+			if err := rows.Scan(&preroute.ID, &preroute.Title, &preroute.Description, &preroute.Full_description, &preroute.Distance,
+				&preroute.Time, &preroute.Tags, &preroute.Total_Checkpoints, &preroute.Images, &preroute.Map, &preroute.Name, &preroute.Lat,
+				&preroute.Ing, &preroute.Route_ID, &preroute.UpdateAt, &preroute.Image, &preroute.Likes); err != nil {
+				log.Println("Scan Error:", err)
+				c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "Data scan error"})
+				return
+			}
+			route.ID = preroute.ID
+			route.Title = preroute.Title
+			route.Description = preroute.Description
+			route.Distance = preroute.Distance
+			route.Time = preroute.Time
+			route.Tags = preroute.Tags
+			route.Likes = preroute.Likes
+			route.Image = preroute.Image
+			route.UpdateAt = preroute.UpdateAt
+
+			routes = append(routes, route)
+		}
+		defer rows.Close()
+
+		// レスポンスを返す
+		response := SearchResponse{
+			HitCount: len(routes),
+			Routes:   routes,
+			Request:  req,
+		}
+		c.IndentedJSON(http.StatusOK, response)
+
+	case "OR":
+		query := "SELECT * FROM routes WHERE (DISTANCE BETWEEN ? AND ?) OR (TIME BETWEEN ? AND ?)"
+		args := []interface{}{req.Distance.Min, req.Distance.Max, req.Time.Min, req.Time.Max}
+
+		rows, err := h.DB.Query(query, args...)
+		if err != nil {
+			log.Println("Query Error:", err)
+			c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "Database query error"})
+			return
+		}
+		defer rows.Close()
+
+		var routes []Route
+		for rows.Next() {
+			var route Route
+			var preroute PreRoute
+			if err := rows.Scan(&preroute.ID, &preroute.Title, &preroute.Description, &preroute.Full_description, &preroute.Distance,
+				&preroute.Time, &preroute.Tags, &preroute.Total_Checkpoints, &preroute.Images, &preroute.Map, &preroute.Name, &preroute.Lat,
+				&preroute.Ing, &preroute.Route_ID, &preroute.UpdateAt, &preroute.Image, &preroute.Likes); err != nil {
+				log.Println("Scan Error:", err)
+				c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "Data scan error" + err.Error()})
+				return
+			}
+			route.ID = preroute.ID
+			route.Title = preroute.Title
+			route.Description = preroute.Description
+			route.Distance = preroute.Distance
+			route.Time = preroute.Time
+			route.Tags = preroute.Tags
+			route.Likes = preroute.Likes
+			route.Image = preroute.Image
+			route.UpdateAt = preroute.UpdateAt
+
+			routes = append(routes, route)
+		}
+
+		// レスポンスを返す
+		response := SearchResponse{
+			HitCount: len(routes),
+			Routes:   routes,
+			Request:  req,
+		}
+		c.IndentedJSON(http.StatusOK, response)
+
 	}
 
-	// 時間のフィルタ
-	if req.Time.Min >= 0 {
-		query += " AND time >= ?"
-		args = append(args, req.Time.Min)
-	}
-	if req.Time.Max >= 0 {
-		query += " AND time <= ?"
-		args = append(args, req.Time.Max)
-	}
 	/*
 		// タグ検索の処理
 		if len(req.Tags) > 0 {
@@ -116,41 +211,6 @@ func (h *SearchHandler) SearchRoutes(c *gin.Context) {
 		}
 		query += " ORDER BY " + sortKey + " " + sortOrder
 	*/
-	// 取得制限
-	limit := 12
-	if req.Limit >= 1 && req.Limit <= 60 {
-		limit = req.Limit
-	}
-	query += " LIMIT ?"
-	args = append(args, limit)
-
-	// クエリ実行
-	rows, err := h.DB.Query(query, args...)
-	if err != nil {
-		log.Println("Query Error:", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database query failed"})
-		return
-	}
-
-	// 結果を格納
-	var routes []Route
-	for rows.Next() {
-		var route Route
-		if err := rows.Scan(&route.ID, &route.Title, &route.Description, &route.Distance, &route.Time, &route.Tags, &route.Likes, &route.Image, &route.UpdateAt); err != nil {
-			log.Println("Scan Error:", err)
-			c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "Data scan error" + err.Error()})
-			return
-		}
-		routes = append(routes, route)
-	}
-	defer rows.Close()
-	// レスポンスを返す
-	response := SearchResponse{
-		HitCount: len(routes),
-		Routes:   routes,
-		Request:  req,
-	}
-	c.IndentedJSON(http.StatusOK, response)
 }
 
 /* リクエスト例 */
